4bfeb82de39263ba898ccdfe5623cbeb86938b83
Abdualaziz Alsharif <62527536+AbdulazizAlsharif@users.noreply.github.com>
2023-05-26 20:50:59 +0300

    test sing

---
+++ Hotel_252/src/hotel_252/DataBaseConnection.java
@@ -0,0 +1,32 @@
+
+package hotel_252;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class DataBaseConnection {
+    // Singielton its only one connection to DB
+    private static DataBaseConnection connnection;
+    private Connection Con;
+    private DataBaseConnection() {
+        try {
+            this.Con=DriverManager.getConnection("jdbc:mariadb://localhost:3306/hotel_252", "root", "2036023");
+        } catch (SQLException ex) {
+            Logger.getLogger(DataBaseConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static DataBaseConnection getconnnection(){
+        if (connnection == null) {
+            connnection =new DataBaseConnection();
+        }
+        return connnection;
+    }
+
+    public Connection getCon() {
+        return Con;
+    }
+    
+}
